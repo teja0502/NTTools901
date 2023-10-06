d967895797930a96d75d307884042758b2090e6f
Suresh <Suresh@gmail.com>
2023-10-06 13:18:49 +0530

    Employee class is updated

--- GitHubProject/src/main/java/com/nt/model/Employee.java
+++ GitHubProject/src/main/java/com/nt/model/Employee.java
@@ -1,5 +1,13 @@
 package com.nt.model;
 
+import lombok.Data;
+
+@Data
 public class Employee {
+	
+	private Integer empNo;
+	private String empName;
+	private String empDept;
+	private Double salary;
 
 }
